c4fa4301b8065d90cf4c1aced83d3fb3a59b1612
NathanaelMarques <nathanaelmarques135@gmail.com>
2023-04-27 09:43:58 -0300

    Piadocas do Tio Chuck

---
+++ src/main/java/com/legend/chucknorrisjokeapp/ChuckNorrisJokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.legend.chucknorrisjokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisJokeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/legend/chucknorrisjokeapp/ChuckNorrisJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legend.chucknorrisjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
